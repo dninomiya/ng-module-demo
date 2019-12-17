4b657e6e4093d27217d4439d5aa72e7ba95cdaa8
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2019-12-17 09:24:53 +0900

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
